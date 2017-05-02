64b9ad622194f3bde8cab45d50c13392bf2c39d8
princeassasin <sriramlakshmim@gmial.com>
2017-05-02 13:08:18 +0530

    Abstract Class initial commit

---
+++ src/com/example/abstractcalculator/java/AbstractClass1.java
@@ -0,0 +1,13 @@
+package com.example.abstractcalculator.java;
+
+public class AbstractClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		add a = new add();
+		a.calculate();
+		
+
+	}
+
+}
